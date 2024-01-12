74f7f15f0959906c779916cc1b101ccfee8ca3a6
Damiano Tagliaferri <ironcutter24@gmail.com>
2024-01-12 18:48:50 +0100

    fix: import custom post content

--- src/partials/BlogPost.tsx
+++ src/partials/BlogPost.tsx
@@ -1,7 +1,8 @@
 import type { IFrontmatter } from 'astro-boilerplate-components';
-import { PostContent, PostHeader, Section } from 'astro-boilerplate-components';
+import { PostHeader, Section } from 'astro-boilerplate-components';
 import type { ReactNode } from 'react';
 
+import { PostContent } from '@/partials/PostContent';
 import { AppConfig } from '@/utils/AppConfig';
 
 type IBlogPostProps = {
