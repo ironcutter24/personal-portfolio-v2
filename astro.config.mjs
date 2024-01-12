040afa98aa5d9716331bf2b54eb1d8423f16df0f
ironcutter24 <ironcutter24@gmail.com>
2024-01-12 02:17:46 +0100

    fix: code highlighting is now showing correctly

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,13 +14,9 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'dracula',
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ['csharp'],
+      theme: 'github-dark',
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
+      wrap: false,
     },
   },
   integrations: [
